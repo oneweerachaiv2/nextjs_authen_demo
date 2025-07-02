ee15411da5f7f3ae2d68ca239a9d3d05ec762c8d
oneweerachaiv2 <one1rich@gmail.com>
2025-07-02 11:39:56 +0700

    Set session expiration to 30 minutes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -51,6 +51,10 @@ export default NextAuth({
   },
   session: {
     strategy: 'jwt',
+    maxAge: 30 * 60, // 30 minutes
+  },
+  jwt: {
+    maxAge: 30 * 60,
   },
   secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === 'development',
